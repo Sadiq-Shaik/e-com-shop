5f5dd4f409c5d459838f7375177854eeb634ba9c
Sadiq Shaik <sadiqshaik@protonmail.com>
2022-12-23 19:10:09 +0530

    added escape characters

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="w-3/4 mx-auto my-6">
       <h2 className="text-2xl my-4">About Me</h2>
-      <h3 className="text-xl">I'm Sadiq Shaik</h3>
+      <h3 className="text-xl">I&apos;m Sadiq Shaik</h3>
       <span className="text-sm">Front-end developer</span>
       <p className="my-4 text-lg">
         I build user-interfaces using React library. Currently learning NextJS
@@ -14,8 +14,8 @@ function About() {
       <p className="my-4 text-lg">
         This web app is build using NextJS framework and MongoDb with
         Authentication and shopping cart funcionality. Although cart data
-        persistance for a particular user in databse hasn't been implemented yet
-        and might be added later along side user-profile page.{" "}
+        persistance for a particular user in databse hasn&apos;t been
+        implemented yet and might be added later along side user-profile page.{" "}
         <a className="underline" href="https://sadiqshaik.com">
           Contact me
         </a>{" "}
